aa169f4e20084b503acee7e6935c17489f10b86f
Tojima689 <tojima.ryuta@headwaters.co.jp>
2024-07-03 15:25:13 +0900

    complete

--- src/util.js
+++ src/util.js
@@ -5,10 +5,12 @@ export function getMonth(month = dayjs().month()) {
     const firstDayOfTheMonth = dayjs(new Date(year, month, 1)).day();
     let currentMonthCount = 0 - firstDayOfTheMonth;
 
-    return new Array(5).fill([]).map(() => {
+    const dayMatrix = new Array(5).fill([]).map(() => {
         return new Array(7).fill(null).map(() => {
             currentMonthCount++;
             return dayjs(new Date(year, month, currentMonthCount));
         });
     });
+
+    return dayMatrix;
 }
